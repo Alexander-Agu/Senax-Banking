c8cb0df05041f17471e938e6631caebf67482560
Alexander-Agu <theonlyagu@gmail.com>
2025-01-22 11:44:26 +0200

    Created the Header component

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import './header.css';
+import { TiThMenuOutline } from "react-icons/ti";
+import { MdClose } from "react-icons/md";
+
+function Header() {
+  return (
+    <header className='header'>
+        <div className="logo">
+            <a href="#">S-B</a>
+        </div>
+
+        <nav className='navbar'>
+            <input type="checkbox" id='sidebar' />
+            <label htmlFor="sidebar" className='menu'> {/* Opens the sidebar */}
+                <TiThMenuOutline />
+            </label>
+
+            <ul className='navList'>
+                <label htmlFor="sidebar" className='close'> {/* Closes the sidebar */}
+                    <MdClose />
+                </label>
+
+
+                <li className='navItem'>
+                    <a href="#" className='navLink'>
+                        Home
+                    </a>
+                </li>
+                <li className='navItem'>
+                    <a href="#" className='navLink'>
+                        About Us
+                    </a>
+                </li>
+                <li className='navItem'>
+                    <a href="#" className='navLink'>
+                        Creators
+                    </a>
+                </li>
+                <li className='navItem'>
+                    <a href="#" className='navLink'>
+                        Contact Us
+                    </a>
+                </li>
+            </ul>
+
+        </nav>
+    </header>
+  )
+}
+
+export default Header
