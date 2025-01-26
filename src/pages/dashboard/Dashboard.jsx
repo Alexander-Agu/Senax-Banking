edfce000f7290ac2979e3c4622b4d8560072f18c
Alexander-Agu <theonlyagu@gmail.com>
2025-01-26 13:53:30 +0200

    Added the account balance and transfer and send cash navigation to dashboard

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { GiEntryDoor } from "react-icons/gi";
 import { SlCalender } from "react-icons/sl";
 import "./dashboard.css"
+import Header from '../../components/header/Header';
 
 function Dashboard() {
+    const transferButton = document.getElementById("T");
+    const sendButton = document.getElementById("S");
+    let [dashPage, setDashPage] = useState(0);
+    let actionPage;
+
+
+    if (dashPage === 0){
+        transferButton.style.backgroundColor = "black";
+        transferButton.style.color = "white";
+
+        sendButton.style.backgroundColor = "#d3d3d3";
+        sendButton.style.color = "black";
+
+        actionPage = <Header />
+    } else if (dashPage === 1){
+        sendButton.style.backgroundColor = "black";
+        sendButton.style.color = "white";
+
+        transferButton.style.backgroundColor = "#d3d3d3";
+        transferButton.style.color = "black";
+
+        actionPage = 0
+    }
+
+    transferButton.onclick = ()=> setDashPage(0);
+    sendButton.onclick = ()=> setDashPage(1)
+
   return (
     <main className='dashboardApp'>
         <nav className='dashSidebar'>
@@ -38,11 +66,23 @@ function Dashboard() {
 
                 <div className="dashActivity">
                     <div className="actTop">
-                        
+                        <h2>Account Balance</h2>
+                        <h2>R2 000 000.00</h2>
                     </div>
 
                     <div className="actBottom">
+                        <h2>Actions</h2>
+                        <nav className='actionNav'>
+                            <button id='T'>
+                                Transfer
+                            </button>
 
+                            <button id='S'>
+                                Send Cash
+                            </button>
+                        </nav>
+
+                        {actionPage}
                     </div>
                 </div>
 
