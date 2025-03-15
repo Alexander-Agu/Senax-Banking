abde55a5bff94f9607b62b1ec14e8c7dedb84c2f
Alexander-Agu <theonlyagu@gmail.com>
2025-03-15 08:41:03 +0200

    Redesigned the hero section

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,11 +1,32 @@
 import React from 'react'
 import "./hero.css"
+import { HiMiniArrowTopRightOnSquare } from "react-icons/hi2";
+
 
 function Hero() {
   return (
-    <main>
-        
-    </main>
+    <section className='heroContainer'>
+      <h1>
+        Experience Elevated Banking
+      </h1>
+
+      <p className='heroMassage'>
+        Senax Banking: Empowering Your Financial Journey with Simplicity, Security, and Smart Solutions for a Brighter Future.
+      </p>
+
+      <div className="directoryButtons">
+        <a href='#' className="toLogin">
+          <p>What are you waiting for</p>
+          <HiMiniArrowTopRightOnSquare />
+        </a>
+
+        <div className="users">
+          <p>
+            50K User's Worldwide
+          </p>
+        </div>
+      </div>
+    </section>
   )
 }
 
