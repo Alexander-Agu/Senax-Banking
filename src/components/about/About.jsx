5efe7c372cfd11187ddb4876196172710f0c987b
Alexander-Agu <theonlyagu@gmail.com>
2025-03-15 14:46:42 +0200

    Used the about models to generate cards on the about component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import "./about.css"
 import Card from '../card/Card'
+import { aboutModels } from '../../models/aboutModels'
 
 function About() {
   return (
     <article className='aboutSection'>
-        <Card />
+        {
+            aboutModels.map(x => {
+                const {title, intro, more, id} = x;
+                return <Card title={title} intro={intro} more={more} id={id} />
+            })
+        }
     </article>
   )
 }
--- src/models/aboutModels.js
+++ src/models/aboutModels.js
@@ -1,15 +1,18 @@
 export const aboutModels = [
     {
+        id: 1,
         title: "Online Money Exchange",
         intro: "Online money exchange in the Senax banking app enables quick and secure currency conversion for international transactions.",
         more: "Senax offers real-time exchange rates, low fees, and strong security features like encryption and two-factor authentication. Users can effortlessly convert currencies for travel, business, or online purchases, ensuring a smooth and transparent experience."
     },
     {
+        id: 2,
         title: "Transection History",
         intro: "Transaction history in the Senax banking app lets users track their spending and account activity in real time.",
         more: "Senax provides a detailed breakdown of past transactions, including dates, amounts, and recipients, helping users stay on top of their spending. With search and filter options, users can quickly find specific transactions, categorize expenses, and track patterns over time."
     },
     {
+        id: 3,
         title: "Account Management",
         intro: "Online money exchange in the Senax banking app enables quick and secure currency conversion for international transactions.",
         more: "With features like balance tracking, transaction history, and fund transfers, Senax ensures seamless banking. Users can set spending limits, manage multiple accounts, and secure their funds with encryption and two-factor authentication."
