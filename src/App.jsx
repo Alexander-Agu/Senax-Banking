29a74051535abb6150a383438802cfc56b08dc2d
Alexander-Agu <theonlyagu@gmail.com>
2025-03-22 16:26:23 +0200

    Changed my reat router

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,33 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route, RouterProvider, createBrowserRouter } from "react-router-dom"
 import Home from "./pages/home/Home"
 import Login from "./components/login/Login"
 import SignUp from "./components/signup/SignUp"
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "/login",
+    element: <Login />
+  },
+  {
+    path: "/signup",
+    element: <SignUp />
+  }
+])
+
 function App() {
   return (
-    <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<SignUp />} />
-        </Routes>
-    </BrowserRouter>
+    <RouterProvider router={router} />
+    // <BrowserRouter>
+    //     <Routes>
+    //         <Route path="/" element={<Home />} />
+    //         <Route path="/login" element={<Login />} />
+    //         <Route path="/signup" element={<SignUp />} />
+    //     </Routes>
+    // </BrowserRouter>
   )
 }
 
