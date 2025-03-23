4ad50f689e9e8d8c6dadda9abea98b8edf31581e
Alexander-Agu <theonlyagu@gmail.com>
2025-03-23 17:58:56 +0200

    Created the transectData component

---
+++ src/components/transectionData/TransectionData.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./transectionData.css"
+
+function TransectionData() {
+  return (
+    <div className="transectionData">
+        <h2>From: MTN</h2>
+        <h2 className='transectMid'>11/05/25</h2>
+        <h2>R4000.00</h2>
+    </div>
+  )
+}
+
+export default TransectionData
